e7501693886f8265e0ecc8d275bbf4ec89f3bce6
hvultur <hvultur@gmail.com>
2020-03-29 17:40:59 +0200

    Enable continuous monitoring

--- voice_record.py
+++ voice_record.py
@@ -7,10 +7,12 @@ import subprocess
 import deepspeech
 import wave
 import numpy
+import argparse
 from pathlib import Path
 from gtts import gTTS
 
 base = '/root/Sw/voice_command/webcrawler/tutorial/tutorial'
+thresh = 10.0
 
 def readjoke(filename):
     f = Path("%s/jokes-%s" % (base, filename))
@@ -22,7 +24,7 @@ def readjoke(filename):
         joke = content[random.randint(0, len(content))]
         return joke
 
-def execute(command):
+def run_command(command):
     if not command:
         subprocess.call("mpg123 unknown.mp3", shell = True)
         return
@@ -34,10 +36,34 @@ def execute(command):
     subprocess.call("mpg123 joke.mp3", shell = True)
     os.remove("joke.mp3")
 
-def main():
-    #subprocess.call("arecord -D hw:0,0 -f cd -t wav -d 3 -r 44100 test.wav", shell = True)
-    subprocess.call("arecord -D hw:3,0 -f cd -t wav -d 3 -r 16000 --channels 1 test.wav", shell = True)
+def decode_wav(filename):
+    #filename = '/root/Downloads/audio/8455-210777-0068.wav'
+    #filename = '/root/Sw/voice_command/test.wav'
 
+    w = wave.open(filename, 'r')
+    rate = w.getframerate()
+    frames = w.getnframes()
+    buffer = w.readframes(frames)
+
+    print(rate)
+
+    data16 = numpy.frombuffer(buffer, dtype=numpy.int16)
+    return data16
+
+def record():
+    subprocess.call("arecord -D hw:0,0 -f cd -t wav -d 3 -r 44100 test.wav", shell = True)
+    #subprocess.call("arecord -D hw:3,0 -f cd -t wav -d 3 -r 16000 --channels 1 test.wav", shell = True)
+
+def just_noise(filename):
+    subprocess.call("sox test.wav -n stats -s 16 2>&1 | awk '/^Max level/ {print $3}' > stats", shell = True)
+    with open("stats") as f:
+        content = f.readlines()
+        if float(content[0]) < thresh:
+            return True
+
+    return False
+
+def prepare_model():
     model_file_path = '/root/Downloads/deepspeech-0.6.0-models/output_graph.pbmm'
     beam_width = 500
     model = deepspeech.Model(model_file_path, beam_width)
@@ -48,19 +74,33 @@ def main():
     lm_beta = 1.85
     model.enableDecoderWithLM(lm_file_path, trie_file_path, lm_alpha, lm_beta)
 
-    #filename = '/root/Downloads/audio/8455-210777-0068.wav'
-    filename = '/root/Sw/voice_command/test.wav'
-    w = wave.open(filename, 'r')
-    rate = w.getframerate()
-    frames = w.getnframes()
-    buffer = w.readframes(frames)
+    return model
 
-    print(rate)
-    print(model.sampleRate())
+def prepare_argparse():
+    parser = argparse.ArgumentParser(description='Optional app description')
+    parser.add_argument('--continuous', action='store_true',
+                        help='continous monitoring')
+    parser.add_argument('--thresh', type=float, help='threshold')
+    return parser
 
-    data16 = numpy.frombuffer(buffer, dtype=numpy.int16)
-    text = model.stt(data16)
-    execute(text)
+def main():
+    parser = prepare_argparse()
+    model = prepare_model()
+
+    args = parser.parse_args()
+
+    while True:
+        record()
+
+        if just_noise('test.wav'):
+            continue
+
+        data16 = decode_wav('test.wav')
+        text = model.stt(data16)
+        run_command(text)
+
+        if not args.continuous:
+            break
 
 main()
 
